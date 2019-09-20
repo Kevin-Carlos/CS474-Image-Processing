134124e784109dc200c41b2cef8767bc1fc9e9e3
Kevin-Carlos <kevin_carlos@nevada.unr.edu>
2019-09-20 00:47:46 -0700

    Working 2 sampling

--- src/driver.cpp
+++ src/driver.cpp
@@ -6,43 +6,103 @@
 int readImageHeader(const char[], int&, int&, int&, bool&);
 int readImage(const char[], ImageType&);
 int writeImage(const char[], ImageType&);
+void sampling(ImageType, ImageType&, const int);
+constexpr int sizeHelper(const int);
 
 int main(int argc, char *argv[]) {
     //int M, N, Q;
 
-    int i, j;
-    int M, N, Q;
-    bool type;
-    int val;
-    int thresh;
-    const char* filePath = "./data_input/peppers.pgm";
+//     int i, j;
+//     int M, N, Q;
+//     bool type;
+  bool type2;
+//     int val;
+//     int thresh;
+  const char* filePath = "./data_input/peppers.pgm";
+  const char *writefilePath = "./data_output/peppers_2sample.pgm";
 
-    readImageHeader(filePath, N, M, Q, type);
+  int Z, X, Y; // for image sampling
+  const int FACTOR = 2; // Change this to make new size
+  int size = 256/FACTOR;
+  int L = size, O = size - 1, P = size;
+  readImageHeader(filePath, Z, X, Y, type2);
+  ImageType samplingImage(Z, X, Y);
+  ImageType resizedImg(L, O, P);
 
-    ImageType image(N, M, Q);
+  readImage(filePath, samplingImage);
 
-    readImage(filePath, image);
+  sampling(samplingImage, resizedImg, FACTOR);
+  writeImage(writefilePath, resizedImg);
 
-    //std::cout << "Enter threshold: ";
-    //std::cin >> thresh;
+//     readImageHeader(filePath, N, M, Q, type);
 
- // threshold image
+//     ImageType image(N, M, Q);
 
- for(i=0; i<N; i++)
-   for(j=0; j<M; j++) {
-     image.getPixelVal(i, j, val);
-     if(val < 50)
-       image.setPixelVal(i, j, 255);
-     else
-       image.setPixelVal(i, j, 0);
-    }
+//     readImage(filePath, image);
 
-   const char *writefilePath = "./data_output/peppers_test.pgm";
+//     //std::cout << "Enter threshold: ";
+//     //std::cin >> thresh;
 
- // write image
- writeImage(writefilePath, image);
+//  // threshold image
+
+//  for(i=0; i<N; i++)
+//    for(j=0; j<M; j++) {
+//      image.getPixelVal(i, j, val);
+//      if(val < 50)
+//        image.setPixelVal(i, j, 255);
+//      else
+//        image.setPixelVal(i, j, 0);
+//     }
+
+//  // write image
+//  writeImage(writefilePath, image);
 
  return (1);
 
+
+}
+
+/**
+ *  Details: Take in an integer defining how much to sample the
+ *            image, e.g. 2. Run through the pixel values and store
+ *            every 2nd pixel value into a new array which will be the
+ *            pixel values of the new image, or every 4th pixel, or 8th.
+ * */
+void sampling(ImageType srcImg, ImageType& newImg, const int FACTOR) {
+  // Variables
+  int srcRows,
+      srcCols,
+      srcLvls;
+
+  // New image variables
+  int newImgCol;
+  int value;
+
+  // test
+  int row, col, lvl;
+
+  // initialization
+  srcImg.getImageInfo(srcRows, srcCols, srcLvls);
+  newImg.getImageInfo(row, col, lvl);
+
+  cout << srcRows << " " << srcCols << " " << srcLvls << " \n";
+  cout << row << " " << col << " " << lvl << " \n";
+
+  // Run thru the image
+  for (int srcImgRow = 0; srcImgRow <= srcRows; srcImgRow++) {
+    if (srcImgRow == 128)
+      break;
+    newImgCol = 0;
+    for (int srcImgCol = 0; srcImgCol <= srcCols; srcImgCol++) {
+      if (srcImgCol % FACTOR == 0) {
+        cout << srcImgRow << " " << newImgCol << " " << value << " \n";
+        srcImg.getPixelVal(srcImgRow, srcImgCol, value);
+        cout << srcImgRow << " " << newImgCol << " " << value << " \n";
+        newImg.setPixelVal(srcImgRow, newImgCol, value);
+
+        newImgCol++;
+      }
+    }
+  }
 
 }
