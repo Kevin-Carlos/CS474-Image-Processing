f63367de9bfbe18d4a5b90f93b294f4f949a4ddb
Adityasidher <adityas@nevada.unr.edu>
2019-09-19 23:11:40 -0700

    Completed output for Question 2

--- src/PA01-Q2.cpp
+++ src/PA01-Q2.cpp
@@ -6,34 +6,91 @@
 #include <sstream>
 #include "../include/image.h"
 
+
+int readImageHeader(const char[], int&, int&, int&, bool&);
+int readImage(const char[], ImageType&);
+void Quantize(int N, int M, int& val, int quantNum, ImageType oldImage, char writePath[]);
 int writeImage(const char[], ImageType&);
 
-
-void Quantize(int N, int M, int& val, int quantNum, ImageType oldImage) 
+//Will output the Quantized image of lena and peppers
+void Quantize(int N, int M, int& val, int quantNum, ImageType oldImage, char writePath[]) 
 {
-
-  char writefilePath[50] = "./data_output/peppers";
-  quantNum = 256/quantNum;
-  std::cout << quantNum << endl;
+  //QuantNum is the number/ratio to scale the rest of the pixels to and also output the corresponding one.
+ 
+  char temporary[50] = ""; 
+  
+  std::stringstream ss;
+  ss << quantNum; //copy the quantNum as a string from int
+  
+  strcpy(temporary, writePath); //take the file to write to and copy it into temporary
+  quantNum = 256/quantNum; //will set the quantNum = #pixels/ratio
+                           //example: for the first question i) 128, it will be 256/128 = 2            
+                           
   
   for(int i=0; i<N; i++)
     for(int j=0; j<M; j++) 
     {
       oldImage.getPixelVal(i, j, val);
-      val /= quantNum; 
+      val /= quantNum;                  //pixel range is now between 128 - 0.
       oldImage.setPixelVal(i, j, val);
     }
 
-  std::stringstream ss;
-  ss << quantNum;
   std::string s = ss.str();
-   
-    
-
-    strcat(writefilePath, s.c_str());
-    strcat(writefilePath, ".pgm");
-
-    std::cout << writefilePath << std::endl;    
-    writeImage(writefilePath, oldImage);
+  strcat(temporary, s.c_str());
+  strcat(temporary, ".pgm");
+  writeImage(temporary, oldImage);
 
 };
+
+void Question2()
+{
+  int N, M, Q; //pepper values
+  int Y, X, Z; //lenna values
+  bool type;
+  int pval; //pepper
+  int lval; //lenna
+
+  const char* PepperfilePath = "./data_input/peppers.pgm";
+  const char* LennafilePath = "./data_input/lenna_1.pgm";
+
+  readImageHeader(PepperfilePath, N, M, Q, type); //
+  readImageHeader(LennafilePath, Y, X, Z, type); //
+
+  ImageType Pepperimage(N, M, Q);
+  ImageType Lennaimage(Y, X, Z);
+
+  readImage(PepperfilePath, Pepperimage);
+  readImage(LennafilePath, Lennaimage);
+    
+  char writePepperPath[50] = "./data_output/peppers";
+  char writeLennaPath[50] = "./data_output/lenna";
+ 
+
+
+  //Will write out the 128, 32, 8, and 2 Quantizations for lenna_1.pgm and pepper.pgm
+  for(int k = 0; k < 4; k++)
+  {
+    switch(k)
+    {
+      case (0):
+        Quantize(N, M, pval, 128, Pepperimage, writePepperPath);
+        Quantize(Y, X, lval, 128, Lennaimage, writeLennaPath);
+        break;
+      case (1):
+        Quantize(N, M, pval, 32, Pepperimage, writePepperPath); 
+        Quantize(Y, X, lval, 32, Lennaimage, writeLennaPath);
+        break;
+      case (2):
+        Quantize(N, M, pval, 8, Pepperimage, writePepperPath);
+        Quantize(Y, X, lval, 8, Lennaimage, writeLennaPath);
+        break;
+      case (3):
+        Quantize(N, M, pval, 2, Pepperimage, writePepperPath);
+        Quantize(Y, X, lval, 2, Lennaimage, writeLennaPath);
+        break;   
+    }
+  }
+ 
+
+
+}
--- src/driver.cpp
+++ src/driver.cpp
@@ -3,47 +3,13 @@
 #include <stdlib.h>
 #include "../include/image.h"
 
-int readImageHeader(const char[], int&, int&, int&, bool&);
-int readImage(const char[], ImageType&);
-void Quantize(int N, int M, int& val, int quantNum, ImageType oldImage);
+void Question2(); //Question 2 for PA01 located in the PA01-Q2.cpp file
+
 
 int main(int argc, char *argv[]) {
-    //int M, N, Q;
-
-    int i, j;
-    int M, N, Q;
-    bool type;
-    int val;
-    int thresh;
-    const char* filePath = "./data_input/peppers.pgm";
-
-  readImageHeader(filePath, N, M, Q, type);
-
-  ImageType image(N, M, Q);
-
-  readImage(filePath, image);
     
-  //const char *writefilePath;
+Question2();
 
-  for(int k = 0; k < 4; k++)
-  {
-    switch(k)
-    {
-      case (0):
-        Quantize(N, M, val, 128, image);
-        break;
-      case (1):
-        Quantize(N, M, val, 32, image);
-        break;
-      case (2):
-        Quantize(N, M, val, 8, image);
-        break;
-      case (3):
-        Quantize(N, M, val, 2, image);
-        break;   
-    }
-  }
- 
 
  return (1);
 
