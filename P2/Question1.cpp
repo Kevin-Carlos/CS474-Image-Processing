dd88dee51c5a1eea62aae4cf451392091673d81c
Kevin Carlos <kevin_carlos@nevada.unr.edu>
2019-10-04 00:03:18 -0700

    File structure for PA02

---
+++ P2/Question1.cpp
@@ -0,0 +1,15 @@
+/**
+ *  Names:      Kevin Carlos and Aditya Sidher
+ * 
+ *  Details:    Implementation for Spatial Filtering (Correlation)
+ * 
+ *  Date due:   10/18/19
+ * 
+ * */
+
+// Function Prototypes
+void Question1();
+
+void Question1() {
+    
+}
---
+++ P2/driver.cpp
@@ -0,0 +1,7 @@
+void Question1();
+
+int main() {
+    Question1();
+
+    return 0; 
+}
