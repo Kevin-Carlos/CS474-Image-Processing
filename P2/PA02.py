2a34e6d4ebde075d5efdd2e916b836579afe7b21
Kevin Carlos <kevin_carlos@nevada.unr.edu>
2019-10-12 02:36:38 -0700

    Wrong.

--- P2/PA02.py
+++ P2/PA02.py
@@ -1,64 +1,6 @@
 from PIL import Image
 import numpy as np
 
-def normalize(width, height, mask):    
-    normalizeVal = width * height
-
-    pixels = list(mask.getdata())
-    pixels = [pixels[i * width: (i+1) * width] for i in range(height)]
-
-    for rows in range(height):
-        for cols in range(width):
-            pixels[rows][cols] = ((pixels[rows][cols] / normalizeVal) * 255)
-            boundCheck(rows, cols, width, height)
-        break
-    return pixels
-
-def boundCheck(currRow, currCol, width, height):
-    #truncate the height and width becasue we start our origin at the center
-    #so if its 5x5 we will be going up and down 2 and left and right 2
-    #that means the topright corner will be technically -2,-2 or
-    #3 from the origin diagnally
-    masktempHeight = height - 2 #example 3 to right go 1 to get to boundary
-    masktempWidth = width - 2
-
-    #actual image width = 5
-    #Actual image width - masktempwidth == 0
-    
-    bounds = []
-    # [left, right, top, bottom]
-
-    #if we are going out of bounds in the left boundary
-    if (tempWidth - currRow < 0):
-        # print("Width - 2: ", tempWidth)
-        # print("Row: ", currRow)
-        bounds.append(1)
-    else:
-        bounds.append(0)
-
-    #if we are going out of bounds in the right boundary
-    if(currRow - tempWidth < 0):
-        bounds.append(1)
-    else:
-        bounds.append(0)
-        
-    #if we are going out of bounds in the top boundary
-    if (tempHeight - currCol < 0):
-        bounds.append(1)
-    else:
-        bounds.append(0)
-
-    #if we are going out of bounds in the bottom boundary
-    if(currCol - tempHeight < 0):
-        # print("Height - 1: ", tempHeight)
-        # print("Column: ", currCol)
-        bounds.append(1)
-    else:
-        bounds.append(0)
-    
-    print(currRow, currCol)
-    print(bounds)
-
 def Correlation(mask_size, mask):
     image = Image.open("./data_input/Image.pgm")
     # im.save("./data_output/Pattern.pgm")
@@ -66,13 +8,65 @@ def Correlation(mask_size, mask):
     width = mask_size[0]
     height = mask_size[1]
 
-    normalizedMask = normalize(width, height, mask)
+    normalizedMask = Normalize(width, height, mask)
 
+    MapCorrelation(mask, image, normalizedMask)
     
 
+def Normalize(width, height, mask):    
+    normalizeVal = width * height
+
+    pixels = list(mask.getdata())
+    pixels = [pixels[i * width: (i+1) * width] for i in range(height)]
+
+    for rows in range(height):
+        for cols in range(width):
+            pixels[rows][cols] = ((pixels[rows][cols] / normalizeVal))
+    return pixels
+
+def MapCorrelation(mask, image, normalizedMask):
+    # Initiffize pixel values
+    imagePixels = list(image.getdata())
+    imagePixels = [imagePixels[i * image.size[0]: (i+1) * image.size[0]] for i in range(image.size[1])]
     
-    # center = []
-    # center.append([width//2, height//2])
+    correlatedMap = Image.new("L", (image.size[0], image.size[1]))
+    # print("Size:", correlatedMap.size)
+    # correlatedPixels = list(correlatedMap.getdata())
+    # correlatedPixels = [correlatedPixels[i * image.size[0]: (i+1) * image.size[0]] for i in range(image.size[1])]
+
+    for i in range(image.size[0]):
+        for j in range(image.size[1]):
+            
+            value = int(ApplyMask(i, j, mask, image, normalizedMask, imagePixels))
+
+            try:
+                correlatedMap.putpixel((i,j), value)
+                # correlatedPixels[i][j] = value / 64.5
+            except:
+                pass
+            
+    # print(correlatedPixels)
+
+    correlatedMap.save("./data_output/Correlation.pgm")
+    
+    
+            
+
+
+def ApplyMask(i, j, mask, image, maskPixels, imagePixels):
+    summation = 0
+    for row in range(-(mask.size[0] // 2), mask.size[0] // 2):
+        for col in range(-(mask.size[1] // 2), mask.size[1] // 2):
+            imageCoordW = i - (mask.size[0] // 2)
+            imageCoordH = j - (mask.size[1] // 2)
+
+            try:
+                summation = summation + (maskPixels[row][col] * imagePixels[imageCoordW][imageCoordH])
+            except:
+                summation = summation + 0
+
+    return summation
+
 
 maskImage = Image.open("./data_input/Pattern.pgm")
 Correlation(maskImage.size, maskImage)
