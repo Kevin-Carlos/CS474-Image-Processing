7e395a337741bdd06c96bde7ed319604b799c6f5
Adityasidher <adityas@nevada.unr.edu>
2019-12-03 17:44:39 -0800

    Added blurring stuff

--- P4/Exp3.py
+++ P4/Exp3.py
@@ -26,31 +26,31 @@ def Experiment3():
 
   pixels = normalizeImage(pixels, height, width, N)
           
-  # # Iterate over all the rows and store it into test2D
-  # pixels = ApplyFFTRow(pixels, width, N, isign)
+  # Iterate over all the rows and store it into test2D
+  pixels = ApplyFFTRow(pixels, width, N, isign)
           
-  # # Iterate over all the columns and store it into pixels
-  # pixels = ApplyFFTCol(pixels, height, N, isign)
+  # Iterate over all the columns and store it into pixels
+  pixels = ApplyFFTCol(pixels, height, N, isign)
 
-  pixels = np.fft.fftshift(np.fft.fft2(pixels))
+  # pixels = np.fft.fftshift(np.fft.fft2(pixels))
 
   minVal = 1000000000000000000000000000
   maxVal = -101010100000000000000000000
 
   pixels = blurImage(pixels, height, width, 1, 0.1, 0.1)
 
-  pixels = np.fft.ifftshift(pixels)
-  pixels = np.real(np.fft.ifft2(pixels))
+  # pixels = np.fft.ifftshift(pixels)
+  # pixels = np.real(np.fft.ifft2(pixels))
   
   # newImage = LinearScaleValues(newImage, maxVal, minVal, pixels, height, width)
   # newImage = LogScaleValues(newImage, maxVal, minVal, pixels, height, width)
 
   ################################## Do the reverse ##################################   
-  # # Iterate over all the columns and store it into pixels
-  # pixels = ApplyFFTCol(pixels, height, N, 1)
+  # Iterate over all the columns and store it into pixels
+  pixels = ApplyFFTCol(pixels, height, N, 1)
 
-  # # Iterate over all the rows and store it into test2D
-  # pixels = ApplyFFTRow(pixels, width, N, 1)
+  # Iterate over all the rows and store it into test2D
+  pixels = ApplyFFTRow(pixels, width, N, 1)
 
   # # pixels = changeAmplitudeSpatial(pixels, height, width, N, 1)
   newImage = LinearScaleValues(newImage, maxVal, minVal, pixels, height, width)
@@ -63,21 +63,26 @@ def blurImage(array, height, width, T, a, b):
 
   for i in range(width):
     for j in range(height):
+      # distanceToCenter = math.sqrt((i - (width//2))**2 + (j - (height//2))**2)
 
-      if(i == 0 and j == 0):
+      # if(i == 0 and j == 0):
+      #   H = 1
+      #   print(H)
+      #   print(array[i][j])
+      # else:
+      #   H = (T / (cmath.pi*(i*a + j*b))) * cmath.sin( cmath.pi * (i*a + j*b)) * cmath.e**( -(cmath.sqrt(-1)) * cmath.pi * (i*a + j*b))
+      try:
+        H = ( T / ( cmath.pi*( ( (i - (width//2)) *a) + ( (j - (height//2))*b) ) ) ) * cmath.sin( cmath.pi * ( ( (i - (width//2)) *a) + ( (j - (height//2))*b) ) ) * cmath.e**( -(cmath.sqrt(-1)) * cmath.pi * ( ( (i - (width//2)) *a) + ( (j - (height//2))*b) ))
+      except:
         H = 1
-        print(H)
-        print(array[i][j])
-      else:
-        H = (T / (cmath.pi*(i*a + j*b))) * cmath.sin( cmath.pi * (i*a + j*b)) * cmath.e**( -(cmath.sqrt(-1)) * cmath.pi * (i*a + j*b))
 
       # H = cmath.e**( -0.0025* (( (i)**2 +  (j)**2)**(5/6)))
 
       Hr = np.real(H)
-      Hi = np.imag(H)
       # G = box_muller(0.0, 1.0)
-      if(j % 2 or i % 2):
-        array[i][j] = array[i][j] * H
+      # if(j % 2 or i % 2):
+      array[i][j] = array[i][j] * Hr
+      array[i][j] = array[i][j] / Hr
 
 
   return array
